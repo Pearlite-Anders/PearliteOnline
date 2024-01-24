7ec29cd8a502fc72b874ec6774bd70a1391e2ceb
Kristian Primdal <kristian@primux.dk>
2024-01-24 11:07:29 +0800

    Update permissions for formulas

--- database/migrations/2024_01_24_014452_create_formulas_table.php
+++ database/migrations/2024_01_24_014452_create_formulas_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -11,8 +12,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Permission::create(['name' => 'project.view']);
-        Permission::create(['name' => 'project.edit']);
+        Permission::create(['name' => 'formula.view']);
+        Permission::create(['name' => 'formula.edit']);
     }
 
     /**
