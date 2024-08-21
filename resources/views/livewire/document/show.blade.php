581d034c1ed1cffcdd1763f49d68044379a1a4b7
Jens Sørensen <jens@speedly.dk>
2024-08-21 10:37:03 +0200

    Very basic show document

--- resources/views/livewire/document/show.blade.php
+++ resources/views/livewire/document/show.blade.php
@@ -1,30 +1,19 @@
 <div>
-    <x-form-header backlink="{{ route('documents.index') }}">
+    <x-form-header backlink="{{ route('welder.index') }}">
         <x-slot name="title">{{ __('View document') }}</x-slot>
     </x-form-header>
-    <div class="bottom-0 right-0 px-4 pt-8 pb-4 border-b-0 border-gray-200 border-solid text-blackborder-t border-x-0">
-        <div class="lg:flex">
-            <nav class="flex flex-col flex-1" aria-label="Subdocuments navigation">
-                <ul role="list" class="px-8 space-y-1">
-                    <x-document-tree-node :document="$treeRoot" :currentId="$document->id" />
-                </ul>
-            </nav>
-            <div class="flex flex-col w-6/12 gap-4">
-                @include('livewire.document._breadcrumb')
-                <div class="flex flex-col flex-1 px-4 py-2 mb-4 leading-7 bg-white rounded-lg shadow xl:px-10 xl:py-8 sm:px-8 sm:py-6">
-                    <h1 class="mb-4 text-3xl font-medium tracking-tight">{{ $revision->data["title"] }}</h1>
-                    <div class="text-base">
-                        {!! $revision->data["content"] !!}
-                    </div>
-                </div>
+    <div class="bottom-0 right-0 px-4 pt-8 pb-4 leading-6 border-b-0 border-gray-200 border-solid text-blackborder-t border-x-0">
+        <div class="md:flex">
+            <div class="container p-4 mb-4 leading-6 text-black bg-white rounded-lg shadow xl:p-8 sm:p-6">
+                {{ $document->data["content"] }}
             </div>
-            <div class="flex grow lg:justify-center text-nowrap">
-                <div class="px-4">
+            <div class="flex justify-center grow">
+                <div>
                     <div class="mb-6">
                         <h2 class="mb-2 text-lg text-gray-900">{{ __('Details') }}</h2>
                         <ul class="text-gray-900 list-none">
                             <li>
-                                Revision: {{ $revision->created_at->format('Y-m-d H:i') }}
+                                Revsion: #1
                             </li>
                             <li>
                                 {{ _('Created at') }}: {{ $document->created_at->format('Y-m-d') }}
@@ -34,74 +23,23 @@
                             </li>
                         </ul>
                     </div>
-                    @if($showActions)
-                        <div>
-                            <h2 class="mb-2 text-lg text-gray-900">{{ __('Actions') }}</h2>
-                            <ul class="text-gray-900 list-none hover:text-black">
-                                @if (auth()->user()->can('update', $document))
-                                    <li>
-                                        <a
-                                        href="{{ route('documents.edit', ['document' => $document->id]) }}"
-                                        class="flex items-center gap-x-4"
-                                        >
-                                            <x-icon.pencil class="w-4 h-4" />
-                                            <span>Edit</span>
-                                        </a>
-                                    </li>
-                                @endif
-                                @if (auth()->user()->can('update', $document))
-                                    <li>
-                                        <a
-                                        href="{{ route('document_revisions.index', ['document' => $document->id]) }}"
-                                        class="flex items-center gap-x-4"
-                                        >
-                                            <x-icon.eye class="w-4 h-4" />
-                                            <span>Revisions</span>
-                                        </a>
-                                    </li>
-                                @endif
-                                @if (auth()->user()->can('update', $document))
-                                    <li>
-                                        <a
-                                        href="{{ route('documents.create', ['parent' => $document->id]) }}"
-                                        class="flex items-center gap-x-4"
-                                        >
-                                            <x-icon.plus class="!h-4 !w-4" />
-                                            <span>Create sub document</span>
-                                        </a>
-                                    </li>
-                                @endif
-                                @if (auth()->user()->can('update', $document))
-                                    <li>
-                                        <div class="flex" x-data @click.prevent.stop="console.log('stop')">
-                                            @if($confirming == true)
-                                                <x-button
-                                                    wire:click="delete()"
-                                                    class="px-1 bg-red-600 hover:bg-red-800"
-                                                >
-                                                    <x-icon.check class="w-4 h-4 text-white" />
-                                                </x-button>
-                                                <x-button
-                                                    wire:click="cancelConfirmDelete"
-                                                    class="bg-cyan-600 hover:bg-cyan-700"
-                                                >
-                                                    <x-icon.x class="w-4 h-4 text-white" />
-                                                </x-button>
-                                            @else
-                                                <button
-                                                    wire:click="confirmDelete()"
-                                                    class="flex items-center text-red-600 bg-transparent gap-x-4 hover:text-red-800"
-                                                >
-                                                    <x-icon.trash class="w-4 h-4" />
-                                                    <span>Delete</span>
-                                                </button>
-                                            @endif
-                                        </div>
-                                    </li>
-                                @endif
-                            </ul>
-                        </div>
-                    @endif
+                    <div>
+                        <h2 class="mb-2 text-lg text-gray-900">{{ __('Actions') }}</h2>
+                        <ul class="text-gray-900 list-none">
+                            <li>
+                                <a href="#" class="flex items-center gap-x-4">
+                                    <x-icon.pencil class="w-4 h-4" />
+                                    <span>Edit</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="flex items-center text-red-600 gap-x-4">
+                                    <x-icon.trash class="w-4 h-4" />
+                                    <span>Delete</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
