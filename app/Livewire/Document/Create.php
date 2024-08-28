64af173be3886951107baa8029634a9fdfc5fb69
Jens Sørensen <jens@speedly.dk>
2024-08-28 09:45:26 +0200

    Add option to create new document

--- app/Livewire/Document/Create.php
+++ app/Livewire/Document/Create.php
@@ -3,44 +3,14 @@
 namespace App\Livewire\Document;
 
 use App\Data\DocumentData;
-use App\Data\PermissionData;
-use App\Models\User;
 use Livewire\Component;
-use Livewire\WithFileUploads;
 use App\Livewire\WithTrixUploads;
-use Illuminate\Support\Collection;
 
 class Create extends Component
 {
-    use WithFileUploads, WithTrixUploads;
+    use WithTrixUploads;
 
     public Form $form;
-    public Collection $users;
-
-    public function mount()
-    {
-        abort_unless(auth()->user()->can('viewAny', Document::class), 403);
-        $this->form->data = DocumentData::from([
-            'title' => '',
-            'default_view' => true,
-            'default_edit' => false
-        ]);
-
-        // TODO: Maybe som pagination here
-        $this->users = auth()->user()->currentCompany->users()->where('role', User::USER_ROLE)->get();
-        $this->form->permissions = $this->users->mapWithKeys(function($user, $key) {
-            return [$user->id => [
-                'name' => $user->name,
-                'view' => true,
-                'edit' => false,
-            ]];
-        })->toArray();
-    }
-
-    public function render()
-    {
-        return view('livewire.document.create');
-    }
 
     public function create()
     {
@@ -50,18 +20,13 @@ class Create extends Component
             ->with('flash.banner', __('Document created.'));
     }
 
-    public function togglePermission($user_index, $permission)
+    public function mount()
     {
-        $this->form->togglePermission($user_index, $permission);
+        $this->form->data = DocumentData::from(['title' => '']);
     }
 
-    public function toggleAllViewPermission()
+    public function render()
     {
-        $this->form->toggleAllViewPermission();
-    }
-
-    public function toggleAllEditPermission()
-    {
-        $this->form->toggleAllEditPermission();
+        return view('livewire.document.create');
     }
 }
