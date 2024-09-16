96da75a718402820af12cd72a1ad6ec5a7322bdc
Jens Sørensen <jens@speedly.dk>
2024-09-16 10:17:54 +0200

    Basic revision of documents

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -6,42 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use App\Models\Trait\HasFilter;
+
 use App\Models\Trait\HasCompany;
 
 class Document extends Model
 {
-    use HasFactory, HasFilter, HasCompany;
+    use HasFactory, HasCompany;
 
     protected $guarded = [];
 
     protected $casts = [
         'data' => 'array',
         'files' => 'array',
-    ];
-
-    public const SYSTEM_COLUMNS = [
-        'title' => [
-            'type' => 'text',
-            'label' => 'Title',
-            'required' => true,
-            'placeholder' => '',
-            'filter' => 'search'
-        ],
-        'introduction' => [
-            'type' => 'textarea',
-            'label' => 'Introduction',
-            'required' => false,
-            'placeholder' => 'A short introduction to the document',
-            'filter' => 'search'
-        ],
-        'content' => [
-            'type' => 'rich_text',
-            'label' => 'Content',
-            'required' => false,
-            'placeholder' => 'The content of this document',
-            'filter' => 'search'
-        ],
     ];
 
     public function owner(): BelongsTo
@@ -57,5 +33,24 @@ class Document extends Model
     public function users()
     {
         return $this->belongsToMany(User::class)->withPivot('view', 'edit')->withTimestamps();
+    }
+
+    public function revisions()
+    {
+        return $this->hasMany(DocumentRevision::class);
+    }
+
+    public function currentRevision()
+    {
+        return $this->hasOne(DocumentRevision::class)->latestOfMany();
+    }
+
+    public function removeOldRevision()
+    {
+        // Remove the oldest 10 revisions. The limit is nessary for MySQL
+        $oldRevisions = $this->revisions()->latest()->skip(10)->limit(9)->get();
+        foreach($oldRevisions as $oldRevision) {
+            $oldRevision->delete();
+        }
     }
 }
--- resources/views/livewire/document/show.blade.php
+++ resources/views/livewire/document/show.blade.php
@@ -5,9 +5,9 @@
     <div class="bottom-0 right-0 px-4 pt-8 pb-4 border-b-0 border-gray-200 border-solid text-blackborder-t border-x-0">
         <div class="lg:flex">
             <div class="w-6/12 px-4 py-2 mb-4 leading-7 bg-white rounded-lg shadow xl:px-10 xl:py-8 sm:px-8 sm:py-6">
-                <h1 class="text-3xl font-medium mb-4 tracking-tight">{{ $document->data["title"] }}</h1>
+                <h1 class="text-3xl font-medium mb-4 tracking-tight">{{ $revision->data["title"] }}</h1>
                 <div class="text-base">
-                    {!! $document->data["content"] !!}
+                    {!! $revision->data["content"] !!}
                 </div>
             </div>
             <div class="flex grow lg:justify-center text-nowrap">
@@ -16,7 +16,7 @@
                         <h2 class="text-lg text-gray-900 mb-2">{{ __('Details') }}</h2>
                         <ul class="list-none text-gray-900">
                             <li>
-                                Revsion: #1
+                                Revision: {{ $revision->created_at->format('Y-m-d H:i') }}
                             </li>
                             <li>
                                 {{ _('Created at') }}: {{ $document->created_at->format('Y-m-d') }}
@@ -26,54 +26,63 @@
                             </li>
                         </ul>
                     </div>
-                    <div>
-                        <h2 class="text-lg text-gray-900 mb-2">{{ __('Actions') }}</h2>
-                        <ul class="list-none text-gray-900 hover:text-black">
-                            @if (auth()->user()->can('update', $document))
-                                <li>
-                                    <a
-                                    href="{{ route('documents.edit', ['document' => $document->id]) }}"
-                                    class="flex gap-x-4 items-center"
-                                    >
-                                        <x-icon.pencil class="h-4 w-4" />
-                                        <span>Edit</span>
-                                    </a>
-                                </li>
-                            @endif
-                            @if (auth()->user()->can('update', $document))
-                                <li>
-                                    <div class="flex" x-data @click.prevent.stop="console.log('stop')">
-                                        @if($confirming == true)
-                                            <x-button
-                                                wire:click="delete()"
-                                                class="bg-red-600 hover:bg-red-800 px-1"
-                                            >
-                                                <x-icon.check class="w-4 h-4 text-white" />
-                                            </x-button>
-                                            <x-button
-                                                wire:click="cancelConfirmDelete"
-                                                class="bg-cyan-600 hover:bg-cyan-700"
-                                            >
-                                                <x-icon.x class="w-4 h-4 text-white" />
-                                            </x-button>
-                                        @else
-                                            <button
-                                                wire:click="confirmDelete()"
-                                                class="bg-transparent flex gap-x-4 items-center text-red-600 hover:text-red-800"
-                                            >
-                                                <x-icon.trash class="w-4 h-4" />
-                                                <span>Delete</span>
-                                            </button>
-                                        @endif
-                                    </div>
-                                    <!-- <a href="#" class="flex gap-x-4 items-center text-red-600">
-                                        <x-icon.trash class="h-4 w-4" />
-                                        <span>Delete</span>
-                                    </a> -->
-                                </li>
-                            @endif
-                        </ul>
-                    </div>
+                    @if($showActions)
+                        <div>
+                            <h2 class="text-lg text-gray-900 mb-2">{{ __('Actions') }}</h2>
+                            <ul class="list-none text-gray-900 hover:text-black">
+                                @if (auth()->user()->can('update', $document))
+                                    <li>
+                                        <a
+                                        href="{{ route('documents.edit', ['document' => $document->id]) }}"
+                                        class="flex gap-x-4 items-center"
+                                        >
+                                            <x-icon.pencil class="h-4 w-4" />
+                                            <span>Edit</span>
+                                        </a>
+                                    </li>
+                                @endif
+                                @if (auth()->user()->can('update', $document))
+                                    <li>
+                                        <a
+                                        href="{{ route('document_revisions.index', ['document' => $document->id]) }}"
+                                        class="flex gap-x-4 items-center"
+                                        >
+                                            <x-icon.eye class="h-4 w-4" />
+                                            <span>Revisions</span>
+                                        </a>
+                                    </li>
+                                @endif
+                                @if (auth()->user()->can('update', $document))
+                                    <li>
+                                        <div class="flex" x-data @click.prevent.stop="console.log('stop')">
+                                            @if($confirming == true)
+                                                <x-button
+                                                    wire:click="delete()"
+                                                    class="bg-red-600 hover:bg-red-800 px-1"
+                                                >
+                                                    <x-icon.check class="w-4 h-4 text-white" />
+                                                </x-button>
+                                                <x-button
+                                                    wire:click="cancelConfirmDelete"
+                                                    class="bg-cyan-600 hover:bg-cyan-700"
+                                                >
+                                                    <x-icon.x class="w-4 h-4 text-white" />
+                                                </x-button>
+                                            @else
+                                                <button
+                                                    wire:click="confirmDelete()"
+                                                    class="bg-transparent flex gap-x-4 items-center text-red-600 hover:text-red-800"
+                                                >
+                                                    <x-icon.trash class="w-4 h-4" />
+                                                    <span>Delete</span>
+                                                </button>
+                                            @endif
+                                        </div>
+                                    </li>
+                                @endif
+                            </ul>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
