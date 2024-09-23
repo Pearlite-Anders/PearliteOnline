e836c1bdd9836b0eceb171c912363df15db0daa5
Kristian Primdal <kristian@primux.dk>
2024-09-23 07:21:58 +0200

    Changed documents location

--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -46,6 +46,15 @@
                             </li>
                         @endcan
 
+                        @can('viewAny', App\Models\Document::class)
+                            <li class="mt-2 mb-0 text-left list-outside">
+                                <x-nav-link href="{{ route('documents.index') }}" :active="request()->routeIs('documents.*')">
+                                    <x-icon.book class="w-5 h-5 mr-2 text-gray-500 align-middle duration-75 ease-in-out" />
+                                    {{ __('Documents') }}
+                                </x-nav-link>
+                            </li>
+                        @endcan
+
                         @if(
                             auth()->user()->can('viewAny', App\Models\WeldingCertificate::class) ||
                             auth()->user()->can('viewAny', App\Models\Wps::class) ||
@@ -208,7 +217,6 @@
                         @endcan
                     </ul>
                 @endif
-
             </div>
         </div>
     </div>
