043213554abfc24fd8f4599e4da28c02efe281cb
Kristian Primdal <kristian@primux.dk>
2024-09-24 10:54:04 +0200

    Refactor DigitalSignature helper and CePrintController
    - Move DigitalSignature class to the helper directory and add it to CePrintController for better organization and reusability.
    - Update DigitalSignature class to use the Illuminate\Support\Facades\Storage facade for improved file handling.
    - Modify the image method in DigitalSignature class to accept a single width parameter instead of width and height.
    - Adjust the font size and color in the image method of DigitalSignature class for better visibility.
    - Add a border to the image in the image method of DigitalSignature class.
    - Scale down the image if the original width is not equal to the specified width in the image method of DigitalSignature class.
    
    Update choices.blade.php to handle non-array options attribute
    
    - Modify the choices.blade.php file to handle cases where the 'options' attribute is not an array.
    
    Improve code readability and performance in CE PDF view
    
    - Refactor the CE PDF view to improve code readability and performance.

--- app/Helpers/DigitalSignature.php
+++ app/Helpers/DigitalSignature.php
@@ -2,8 +2,9 @@
 
 namespace App\Helpers;
 
-use Intervention\Image\Drivers\Imagick\Driver;
 use Intervention\Image\ImageManager;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Drivers\Imagick\Driver;
 
 class DigitalSignature
 {
@@ -13,20 +14,21 @@ class DigitalSignature
         $base64 = false,
         $hide_time = false,
         $hide_date = false,
-        $width = 302,
-        $height = 72
-    ): string
-    {
+        $width = 302
+    ): string {
         $date = now()->setTimezone('Europe/Copenhagen');
+        $originalWidth = 302;
+        $originalHeight = 72;
+
 
         // create new manager instance with desired driver
         $manager = new ImageManager(Driver::class);
-        $image = $manager->create($width, $height);
+        $image = $manager->create($originalWidth, $originalHeight);
 
-        $image->text($header, 5, 22, function ($font) {
+        $image->text($header, 5, 15, function ($font) {
             $font->filename(resource_path('fonts/Roboto-Bold.ttf'));
-            $font->size(22);
-            $font->color('#000');
+            $font->size(12);
+            $font->color('#0891b2');
         });
 
         $image->text($name, 5, 45, function ($font) {
@@ -41,6 +43,17 @@ class DigitalSignature
                 $font->size(18);
                 $font->color('#000');
             });
+        }
+
+        // Add a border to the image
+        $image->drawRectangle(0, 0, function ($draw) use ($originalWidth, $originalHeight) {
+            $draw->size($originalWidth - 1, $originalHeight - 1);
+            $draw->border('#0891b2', 2);
+        });
+
+        // if originalWidth is not equal to width
+        if ($originalWidth !== $width) {
+            $image->scaleDown($width);
         }
 
         if ($base64) {
