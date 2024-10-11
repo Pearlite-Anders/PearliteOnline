5d5e625f5d35d53469f70a256a037711659fd4a0
Jens Sørensen <jens@speedly.dk>
2024-10-11 10:49:41 +0200

    Use wire:naviate on task cards

--- resources/views/livewire/dashboard/_machine_maintenance_task.blade.php
+++ resources/views/livewire/dashboard/_machine_maintenance_task.blade.php
@@ -1,4 +1,4 @@
-<a class="" href="{{ route('machine-maintenance.edit', ['machineMaintenance' => $task->id]) }}">
+<a class="" href="{{ route('machine-maintenance.edit', ['machineMaintenance' => $task->id]) }}" wire:navigate>
     <div class="px-4 py-3 sm:px-6 sm:py-4">
         <span class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10 mb-3">{{ _('Maintenance') }}</span>
         <p>{{ __('Its time for maintenance of') }}: {{ $task->data['name'] }}</p>
