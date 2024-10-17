07d9696eced2db43ab5f39d60a6409e8e3db5f89
Kristian Primdal <kristian@primux.dk>
2024-10-17 15:01:04 +0200

    Refactor toggle input component

--- resources/views/components/input/toggle.blade.php
+++ resources/views/components/input/toggle.blade.php
@@ -7,7 +7,7 @@
     x-data="{ value: {{ $initial_value ? 'true' : 'false'}}}"
     class="flex items-center space-x-4 md:space-x-0"
 >
-    {{ $label ?? '' }}
+
     <button
         x-ref="toggle"
         @click="value = ! value; $wire.set('{{ $attributes['wire:model'] }}', value)"
