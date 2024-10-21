c0c396fbc6d81cb1d65acc4763b8ea946624d2da
Jens Sørensen <jens@speedly.dk>
2024-10-21 13:26:27 +0200

    Show Supplier and MachineMaintenance reports as table

--- resources/views/livewire/machine-maintenance/edit.blade.php
+++ resources/views/livewire/machine-maintenance/edit.blade.php
@@ -18,8 +18,8 @@
     </form>
 
     <div class="bottom-0 right-0 px-4 pt-8 pb-4 leading-6 border-b-0 border-gray-200 border-solid text-blackborder-t border-x-0">
-        <div class="p-4 mb-4 leading-6 text-black bg-white rounded-lg shadow xl:p-8 sm:p-6">
-            <div class="flex justify-between mx-0 mt-0 mb-4">
+        <div class="py-4 mb-4 leading-6 text-black bg-white rounded-lg shadow xl:py-8 sm:py-6">
+            <div class="flex justify-between mx-0 mt-0 mb-4 px-4 xl:px-8 sm:px-6">
                 <h3 class=" text-xl font-bold leading-7">
                     {{ __('Machine maintenances') }}
                 </h3>
@@ -30,38 +30,7 @@
                 </div>
             </div>
 
-            <ul role="list" class="space-y-6">
-                @forelse($machineMaintenance->reports as $report)
-                    <li class="relative flex gap-x-4">
-                        @unless($loop->last)
-                            <div class="absolute top-0 left-0 flex justify-center w-6 -bottom-6">
-                                <div class="w-px bg-gray-200"></div>
-                            </div>
-                        @endif
-                        <div class="relative flex items-center justify-center flex-none w-6 h-6 bg-white">
-                            <div class="h-1.5 w-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300"></div>
-                        </div>
-                        <div class="flex items-center flex-auto py-0.5 text-sm leading-5 text-gray-500">
-                            <span class="mr-2 font-medium text-gray-900">{{ $report->user->name }}</span>
-                            {{ __('created this maintenance.') }}
-
-                            @if($report->current_file_id)
-                                @php($file = App\Models\File::find($report->current_file_id))
-                                <div class="ml-2">
-                                    <x-file-with-modal :file="$file" svg_location="left" icon_class="w-4 h-4" />
-                                </div>
-                            @endif
-                        </div>
-                        <div class="flex-none py-0.5 text-sm leading-5 text-gray-500">
-                            {{ $report->data['maintenance_date'] }}
-                        </div>
-                    </li>
-                @empty
-                <li class="text-gray-500">
-                    {{ __('No maintenance yet.') }}
-                </li>
-                @endforelse
-            </ul>
+            <x-reports :reports="$machineMaintenance->reports" />
 
             <!-- New maintenance form -->
             @if($maintenanceFormOpen)
