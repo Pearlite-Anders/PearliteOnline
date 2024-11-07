927b08e8fe572b8085c9d0e234154e6e48e2b5d9
Jens Sørensen <jens@speedly.dk>
2024-11-07 13:47:13 +0100

    Fix merge conflict

--- app/Livewire/RoutineInspection/Shared.php
+++ app/Livewire/RoutineInspection/Shared.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<<< HEAD:app/Livewire/RoutineInspection/Shared.php
 namespace App\Livewire\RoutineInspection;
-========
-namespace App\Livewire\Backoffice\ContactPerson;
->>>>>>>> task_27:app/Livewire/Backoffice/ContactPerson/Shared.php
 
 use Livewire\Attributes\On;
 use Illuminate\Support\Carbon;
