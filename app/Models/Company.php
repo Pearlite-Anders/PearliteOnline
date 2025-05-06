ab1829e0c94655da27d18ab59c28886c6fe4fbac
Kristian Primdal <kristian@primux.dk>
2025-05-06 15:43:40 +0200

    Refactor Company model: move 'number' column definition and clean up code formatting

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -78,12 +78,6 @@ class Company extends Model
             'placeholder' => '',
             'filter' => 'search'
         ],
-        'number' => [
-            'type' => 'text',
-            'label' => 'Number',
-            'required' => true,
-            'filter' => 'search'
-        ],
     ];
 
     public function users()
